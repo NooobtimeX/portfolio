04eab5a9fc697d1f610b10c0a631fc97418c1481
NooobtimeX <nooobtimex@gmail.com>
2024-08-08 09:14:30 +0700

    photo local file

--- components/skill.tsx
+++ components/skill.tsx
@@ -1,85 +1,51 @@
-import {
-  SiNextdotjs,
-  SiNuxtdotjs,
-  SiPrisma,
-  SiSqlite,
-  SiPostgresql,
-  SiReact,
-  SiVuedotjs,
-  SiVercel,
-  SiNodedotjs,
-  SiTailwindcss,
-  SiDaisyui,
-  SiPlesk,
-  SiPython,
-  SiJavascript,
-  SiTypescript,
-} from "react-icons/si";
 import React from "react";
 
-const iconStyle = { fontSize: "3rem" };
-
-const SkillIcon = ({ Icon }: { Icon: React.ElementType }) => (
-  <Icon style={iconStyle} />
-);
+const iconStyle = { width: "3rem", height: "3rem" };
 
 const skills = [
   {
     category: "Frontend",
     skills: [
-      { Icon: SiNextdotjs, name: "Next.js" },
-      { Icon: SiNuxtdotjs, name: "Nuxt.js" },
-      { Icon: SiReact, name: "React" },
-      { Icon: SiVuedotjs, name: "Vue.js" },
-      { Icon: SiTailwindcss, name: "Tailwind CSS" },
-      { Icon: SiDaisyui, name: "DaisyUI" },
+      { src: "/logo/nextjs.ico", name: "Next.js" },
+      { src: "/logo/nuxtjs.png", name: "Nuxt.js" },
+      { src: "/logo/react.ico", name: "React" },
+      { src: "/logo/vuejs.svg", name: "Vue.js" },
+    ],
+  },
+  {
+    category: "CSS Framework",
+    skills: [
+      { src: "/logo/tailwindcss.png", name: "Tailwind CSS" },
+      { src: "/logo/daisyui.ico", name: "DaisyUI" },
+      { src: "/logo/bootstrap.ico", name: "Bootstrap" },
     ],
   },
   {
     category: "Backend",
     skills: [
-      { Icon: SiPrisma, name: "Prisma" },
-      { Icon: SiSqlite, name: "SQLite" },
-      { Icon: SiPostgresql, name: "PostgreSQL" },
-      { Icon: SiNodedotjs, name: "Node.js" },
+      { src: "/logo/prisma.png", name: "Prisma" },
+      { src: "/logo/sqlite.png", name: "SQLite" },
+      { src: "/logo/postgresql.ico", name: "PostgreSQL" },
+      { src: "/logo/nodejs.png", name: "Node.js" },
     ],
   },
   {
-    category: "Deployment",
+    category: "Programming Language",
     skills: [
-      { Icon: SiVercel, name: "Vercel" },
-      { Icon: SiPlesk, name: "Plesk" },
+      { src: "/logo/python.ico", name: "Python" },
+      { src: "/logo/javascript.png", name: "Javascript" },
+      { src: "/logo/typescript.ico", name: "Typescript" },
     ],
   },
   {
-    category: "Language",
+    category: "Soft Skills",
     skills: [
-      { Icon: SiPython, name: "Python" },
-      { Icon: SiJavascript, name: "Javascript" },
-      { Icon: SiTypescript, name: "Typescript" },
+      { src: "/logo/chatgpt.png", name: "Chat GPT" },
+      { src: "/logo/copilot.png", name: "Copilot" },
+      { src: "/logo/gemini.svg", name: "Gemini" },
     ],
   },
 ];
-
-const SkillGrid = ({
-  skills,
-}: {
-  skills: Array<{ Icon: React.ElementType; name: string }>;
-}) => (
-  <div className="flex flex-wrap justify-center gap-6 mb-8">
-    {skills.map((skill, index) => (
-      <div
-        key={index}
-        className="flex flex-col justify-center items-center h-24 p-4 shadow-lg rounded-lg transform transition-transform hover:scale-105 w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/ xl:lg:w-1/6 2xl:lg:w-1/12 min-w-[150px] min-h-[100px]"
-      >
-        <div className="text-6xl mb-2">
-          <SkillIcon Icon={skill.Icon} />
-        </div>
-        <span className="text-lg font-semibold">{skill.name}</span>
-      </div>
-    ))}
-  </div>
-);
 
 export default function Skill() {
   return (
@@ -89,7 +55,22 @@ export default function Skill() {
           <h2 className="text-2xl font-bold mb-6">
             {category.category} Skills
           </h2>
-          <SkillGrid skills={category.skills} />
+          <div className="flex flex-wrap justify-center gap-6 mb-8">
+            {category.skills.map((skill, index) => (
+              <div
+                key={index}
+                className="flex flex-col justify-center items-center h-24 p-4 shadow-lg rounded-lg transform transition-transform hover:scale-110 min-w-[150px] min-h-[100px]"
+              >
+                <img
+                  src={skill.src}
+                  alt={skill.name}
+                  style={iconStyle}
+                  className="rounded-lg"
+                />
+                <span className="text-lg font mt-2">{skill.name}</span>
+              </div>
+            ))}
+          </div>
         </React.Fragment>
       ))}
     </div>
