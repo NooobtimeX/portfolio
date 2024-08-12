e4bf7dbfd684882e92ef3e1e2265624083729234
NooobtimeX <nooobtimex@gmail.com>
2024-08-12 21:34:39 +0700

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +17,10 @@ export const metadata: Metadata = {
     "Wongsaphat Puangsorn, NooobtimeX, Thammasat University, Software Developer, Thailand, Portfolio, Resume",
   icons: "/favicon.ico",
   robots: "index, follow",
+  verification: {
+    google: "KiAn5R5UAuZgmwSS_KpMOO2FIRmt-39QIKrHKXrAOL8",
+  },
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,13 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <meta
-          name="google-site-verification"
-          content="KiAn5R5UAuZgmwSS_KpMOO2FIRmt-39QIKrHKXrAOL8"
-        />
-        <GoogleTagManager gtmId="GTM-5PVXPTWP" />
-      </Head>
+      <GoogleTagManager gtmId="GTM-5PVXPTWP" />
       <body className={inter.className}>
         <Header />
         <main className="mb-20 mt-2">{children}</main>
