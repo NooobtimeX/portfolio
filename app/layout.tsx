86cd6206c251b82a124f9ebcec903c513f084e20
NooobtimeX <nooobtimex@gmail.com>
2024-08-12 21:25:04 +0700

    fix bug due to cant deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
-import { Head } from "next/document";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
