97965628e51aeaf12e750e3e46da4cb6a7c943a3
NooobtimeX <nooobtimex@gmail.com>
2024-08-12 21:22:48 +0700

    google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { Head } from "next/document";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +27,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-5PVXPTWP" />
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="KiAn5R5UAuZgmwSS_KpMOO2FIRmt-39QIKrHKXrAOL8"
+        />
+        <GoogleTagManager gtmId="GTM-5PVXPTWP" />
+      </Head>
       <body className={inter.className}>
         <Header />
         <main className="mb-20 mt-2">{children}</main>
