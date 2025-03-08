3f4c48c401add5cc02ff0ae5b01ccb399d493645
NooobtimeX <nooobtimex@gmail.com>
2025-03-09 02:02:30 +0700

    Change way to display skill

--- components/section/Skills.tsx
+++ components/section/Skills.tsx
@@ -1,14 +1,17 @@
 "use client";
 
+import SectionTransition from "@/components/SectionTransition";
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import { CgMonday } from "react-icons/cg";
+import { FaDatabase, FaGitAlt, FaLaptopCode } from "react-icons/fa";
+import { GoProject } from "react-icons/go";
 import {
   SiBootstrap,
   SiClickup,
   SiDaisyui,
   SiGithub,
   SiGitlab,
-  SiJavascript,
-  SiMui,
   SiMysql,
   SiNextdotjs,
   SiNodedotjs,
@@ -17,14 +20,9 @@ import {
   SiPrisma,
   SiReact,
   SiShadcnui,
-  SiSqlite,
   SiTailwindcss,
-  SiTypescript,
   SiVuedotjs,
 } from "react-icons/si";
-
-import SectionTransition from "@/components/SectionTransition";
-import { Card } from "@/components/ui/card";
 
 type Skill = {
   name: string;
@@ -33,45 +31,39 @@ type Skill = {
 
 type SkillGroup = {
   groupName: string;
+  Icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   skills: Skill[];
 };
 
-const frontendFramework: SkillGroup = {
-  groupName: "Frontend Framework",
+const frontendDevelopment: SkillGroup = {
+  groupName: "Frontend Development",
+  Icon: FaLaptopCode,
   skills: [
     { name: "NEXT JS", Icon: SiNextdotjs },
     { name: "NUXT JS", Icon: SiNuxtdotjs },
     { name: "REACT JS", Icon: SiReact },
     { name: "VUE JS", Icon: SiVuedotjs },
-    { name: "JAVASCRIPT", Icon: SiJavascript },
-    { name: "TYPESCRIPT", Icon: SiTypescript },
-  ],
-};
-
-const frontendCSS: SkillGroup = {
-  groupName: "Frontend CSS",
-  skills: [
     { name: "BOOTSTRAP", Icon: SiBootstrap },
     { name: "TAILWIND CSS", Icon: SiTailwindcss },
     { name: "DAISY UI", Icon: SiDaisyui },
     { name: "SHAD/CN UI", Icon: SiShadcnui },
-    { name: "MUI", Icon: SiMui },
   ],
 };
 
 const backendDevelopment: SkillGroup = {
   groupName: "Backend Development",
+  Icon: FaDatabase,
   skills: [
     { name: "NODE JS", Icon: SiNodedotjs },
     { name: "MYSQL", Icon: SiMysql },
     { name: "POSTGRESQL", Icon: SiPostgresql },
-    { name: "SQLITE", Icon: SiSqlite },
     { name: "PRISMA", Icon: SiPrisma },
   ],
 };
 
 const versionControl: SkillGroup = {
   groupName: "Version Control",
+  Icon: FaGitAlt,
   skills: [
     { name: "GITHUB", Icon: SiGithub },
     { name: "GITLAB", Icon: SiGitlab },
@@ -80,6 +72,7 @@ const versionControl: SkillGroup = {
 
 const managementTools: SkillGroup = {
   groupName: "Management Tools",
+  Icon: GoProject,
   skills: [
     { name: "MONDAY", Icon: CgMonday },
     { name: "CLICK UP", Icon: SiClickup },
@@ -87,21 +80,23 @@ const managementTools: SkillGroup = {
 };
 
 function SkillGroupCard({ group }: { group: SkillGroup }) {
+  const GroupIcon = group.Icon;
   return (
-    <Card className="h-full">
+    <Card className="min-h-full">
+      <div className="flex justify-center mt-4">
+        <GroupIcon className="h-10 w-10" />
+      </div>
       <h3 className="mb-2 text-2xl font-semibold text-center">
         {group.groupName}
       </h3>
-      <div className="flex flex-wrap justify-center gap-6">
+      <div className="flex flex-wrap justify-center gap-2">
         {group.skills.map((skill, index) => {
-          const Icon = skill.Icon;
+          const SkillIcon = skill.Icon;
           return (
-            <div key={index}>
-              <div className="flex flex-col items-center justify-center p-2">
-                <Icon className="h-8 w-8 md:h-9 md:w-9 lg:h-10 lg:w-10 xl:h-11 xl:w-11 2xl:h-12 2xl:w-12" />
-              </div>
-              <div className="text-center mx-auto">{skill.name}</div>
-            </div>
+            <Button key={index} variant="outline">
+              <SkillIcon className="h-6 w-6 md:h-8 md:w-8 lg:h-10 lg:w-10" />
+              {skill.name}
+            </Button>
           );
         })}
       </div>
@@ -116,27 +111,15 @@ export default function Skill() {
         <h2 className="text-center text-4xl font-bold text-primary mb-8">
           SKILL
         </h2>
-        <div className="grid gap-4 grid-cols-1 md:gri d-cols-2 lg:grid-cols-3">
+        <div className="grid gap-4 grid-cols-1">
           <SectionTransition>
-            <SkillGroupCard group={frontendFramework} />
+            <SkillGroupCard group={frontendDevelopment} />
           </SectionTransition>
           <SectionTransition>
-            <SkillGroupCard group={frontendCSS} />
+            <SkillGroupCard group={backendDevelopment} />
           </SectionTransition>
-          <div className="hidden lg:block">
-            <SectionTransition>
-              <SkillGroupCard group={backendDevelopment} />
-            </SectionTransition>
-          </div>
         </div>
-        <div className="grid gap-4 grid-cols-1 mt-4 lg:m-0">
-          <div className="lg:hidden block">
-            <SectionTransition>
-              <SkillGroupCard group={backendDevelopment} />
-            </SectionTransition>
-          </div>
-        </div>
-        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 mt-4  ">
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 mt-4">
           <SectionTransition>
             <SkillGroupCard group={versionControl} />
           </SectionTransition>
